bbf9976d3ca969215c0514237edff1c192347c6f
Mehdi Bernoussi <mehdiber@amazon.com>
2023-04-27 02:22:21 +0000

    path-parameters

--- accounting_api/app.py
+++ accounting_api/app.py
@@ -1,7 +1,20 @@
 from fastapi import FastAPI
+import boto3
 
 app = FastAPI()
+
 
 @app.get("/")
 def home():
     return {"message": "Hello World"}
+
+
+ddb_resource = boto3.resource('dynamodb')
+
+
+@app.get("/employees/{employee_id}")
+def get_employee(employee_id: int):
+    response = ddb_resource.Table('employees').get_item(
+        Key={'employee_id': employee_id}
+    )
+    return response['Item']
