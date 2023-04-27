765369b9128b4decbdb5c8b669545d37c0d9a606
Mehdi Bernoussi <mehdiber@amazon.com>
2023-04-27 02:33:59 +0000

    add request body

--- accounting_api/app.py
+++ accounting_api/app.py
@@ -18,3 +18,19 @@ def get_employee(employee_id: int):
         Key={'employee_id': employee_id}
     )
     return response['Item']
+
+
+@app.post("/employees")
+def create_employee(
+    employee_id: int, first_name: str, last_name: str, country: str, salary: int
+):
+    response = ddb_resource.Table('employees').put_item(
+        Item={
+            'employee_id': employee_id,
+            'first_name': first_name,
+            'last_name': last_name,
+            'country': country,
+            'salalry': salary,
+        }
+    )
+    return response
